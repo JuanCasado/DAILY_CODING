61423d0b49bce24f6a17e4a09bb6cbbd2eafb1ee
Juan Casado Ballesteros <juan.casado@edu.uah.es>
2019-01-06 16:26:00 +0100

    DAY9

--- DAY9/UNIVAL_TREE.hpp
+++ DAY9/UNIVAL_TREE.hpp
@@ -1,5 +1,32 @@
 #ifndef UNIVAL_TREE_HPP
 #define UNIVAL_TREE_HPP
+#include <vector>
+#include <string>
+#include <sstream>
+#include <memory>
 
+class Tree {
+	public :
+		enum Branch{
+			kleft,
+			kright
+		};
+		std::string serialize ();
+		static std::unique_ptr<Tree> unSerialize (const std::string &tree);
+		void addNode (const std::string &node_attachment,const std::string &new_node, const Branch &branch);
+		std::string draw(std::string indent = "", bool first = true);
+		Tree (const std::string &val, std::unique_ptr<Tree> &&left = nullptr, std::unique_ptr<Tree> &&right = nullptr) : _val(val), _left(std::move(left)), _right(std::move(right)) {}
+		int countUnival ();
+		int countLeafs ();
+	private:
+		int countUnival (bool &isUnival);
+		static const std::string kseparator;
+		static const std::string knode_null;
+		std::string _val;
+		std::unique_ptr<Tree> _left;
+		std::unique_ptr<Tree>  _right;
+		void attachNode (const std::string &new_node, const Branch &branch);
+		static std::vector<std::string> parseTreeString (std::string tree);
+};
 
 #endif
--- DAY9/main.cc
+++ DAY9/main.cc
@@ -1,5 +1,15 @@
 #include "UNIVAL_TREE.hpp"
+#include <iostream>
 
 int main(int argc, char const *argv[]) {
+	std::unique_ptr<Tree> root = std::make_unique<Tree>(Tree{"root"});
+	root->addNode("root", "left", Tree::Branch::kleft);
+	root->addNode("root", "node", Tree::Branch::kright);
+	root->addNode("node", "right", Tree::Branch::kright);
+	root->addNode("node", "left", Tree::Branch::kleft);
+	root->addNode("right", "right", Tree::Branch::kright);
+	root->addNode("right", "right", Tree::Branch::kleft);
+	std::cout << root->draw() << std::endl;
+	std::cout << root->countUnival() << std::endl;
 	return 0;
 }
